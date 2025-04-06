5a70f60bb84f2749da84b0c8394dffbdb76469c8
Sidopjescherm <hermussidney@gmail.com>
2025-04-06 22:05:53 +0200

    #4

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 import express from 'express'
 
+import { Liquid } from 'liquidjs';
+
+import { readdir, readFile } from 'node:fs/promises'
+
+const files = await readdir('content')
+
+console.log(files)
+
 const app = express()
 
+app.set('views', './views')
+
 app.use(express.static('public'))
+
+const engine = new Liquid();
+app.engine('liquid', engine.express()); 
+
+// ----Hier is de homepage---
+app.get('/', async function(request, response){
+    response.render('index.liquid', {files: files})
+})
+
+app.get('/:slug', async function(req, res) {
+    console.log(req.params.slug)
+    const fileContents = await readFile('/' + req.params.slug + '.md', { encoding: 'utf8' })
+    res.render('artikel.liquid', {fileContent: fileContents})
+  })
+
+// ---Hier is de warhammer pagina---
+app.get('/warhammer', async function (request, response) {
+    response.render('warhammer.liquid')
+})
 
 app.use(express.urlencoded({extended: true}))
 
