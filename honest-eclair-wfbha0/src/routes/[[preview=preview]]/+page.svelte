a4e069256ddbef51641a150d48177999ad4d45b3
Amir Amerian <amirnapoletano@gmail.com>
2025-04-17 19:01:58 +0200

    Delete non important files

--- honest-eclair-wfbha0/src/routes/[[preview=preview]]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { SliceZone } from '@prismicio/svelte';
-
-	import { components } from '$lib/slices';
-
-	export let data;
-</script>
-
-<SliceZone slices={data.page.data.slices} {components} />
