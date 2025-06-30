dcaf36e8544421885d44ec7e4b64a80227ac7dba
Sidopjescherm <hermussidney@gmail.com>
2025-06-30 13:24:25 +0200

    #12 Onnodige bestanden

--- public/server_spells.js
+++
@@ -1,9 +0,0 @@
-const api_spells = "https://www.dnd5eapi.co/api/spells"
-
-// hier maak ik informatie uit de spells API
-const spellsResponse = await fetch(api_spells);
-
-// nu maak ik er een JSON object van
-const spellsResponseJSON = await spellsResponse.json();
-
-// Hier zet ik de data van de spells naar de pagina
